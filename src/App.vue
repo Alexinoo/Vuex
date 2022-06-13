9c9f2ad9e2d8a28cdd2a2bda81e9022c8918bac1
Alex Mwangi <alex@stl-horizon.com>
2022-06-13 11:43:16 +0300

    Using a Vuex store in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
+<!--
+
+-Then we use $store.state.counter to access the value of our counter
+
+   > $store - points at the Vuex store ; 
+    
+   > .state - Has a state property which points at the state object managed by the store 
+
+   > property - Actual property value
+
+ -->
+
+
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <h3>{{ $store.state.counter }}</h3>
+    <button @click="addOne">Add 1</button>
+  </base-container>
 </template>
 
 <script>
